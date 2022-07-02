7f6aac54279b061f7ee3f607a30dd709e199bfae
myriamroxanamedina <104227876+myriamroxanamedina@users.noreply.github.com>
2022-07-02 20:25:50 -0300

    Add files via upload

---
+++ imagenes/javascript.js
@@ -0,0 +1,18 @@
+
+
+function inclusive()
+{
+document.body.style.backgroundColor = "black";
+document.getElementById("main").style.backgroundColor = "black";
+document.getElementById("header").style.backgroundColor = "black";
+document.getElementById("header").style.color = "#90ffe9";
+document.getElementById("nav").style.backgroundColor = "whitesmoke";
+document.getElementById("nav").style.color = "black";
+document.getElementById("section").style.backgroundColor = "black";
+document.getElementById("section").style.color = "whitesmoke";
+document.getElementById("aside").style.backgroundColor = "black";
+document.getElementById("footer").style.backgroundColor = "black";
+}
+function copy() {
+  alert ("Autora: Myriam Roxana Medina. Email : myriamrmedina@gmail.com");
+}
